6eebc06a06528ccf4c1209b8c2dca89a8c7b1294
Louise McCafferty <mccafferty_@hotmail.com>
2022-10-19 00:49:15 +0100

    final commit

---
+++ scope_homework.js
@@ -0,0 +1,31 @@
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Library',
+    weapon: 'Rope'
+  };
+  
+  const declareMurderer = function() {
+    return `The murderer is ${scenario.murderer}.`;
+  }
+  
+  const verdict = declareMurderer();
+  console.log(verdict);
+
+//   "The murderer is Miss Scarlett"
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// ""
+
